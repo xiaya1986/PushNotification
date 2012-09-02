f30225046579dd5445479785c984f4e6913fa159
Ken Xu <xuyinjie2005@hotmail.com>
2012-09-02 23:19:55 +0800

    Finish EX1
    Finish Ex1 and add Ex2,3 source codes

---
+++ Ex1-RawNotifications/Begin/WPPushNotification.TestClient/MainPage.xaml.cs
@@ -0,0 +1,162 @@
+﻿// ----------------------------------------------------------------------------------
+// Microsoft Developer & Platform Evangelism
+// 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+// ----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Notification;
+using System.Diagnostics;
+using System.Windows.Threading;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+
+namespace WPPushNotification.TestClient
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+        private HttpNotificationChannel httpChannel;
+        const string channelName = "WeatherUpdatesChannel";
+        const string fileName = "PushNotificationsSettings.dat";
+        const int pushConnectTimeout = 30;
+
+		// Constructor
+		public MainPage()
+		{
+			InitializeComponent();
+
+			ConnectToMSPN();
+		}
+
+		#region Tracing and Status Updates
+		private void UpdateStatus(string message)
+		{
+			txtStatus.Text = message;
+		}
+
+		private void Trace(string message)
+		{
+#if DEBUG
+			Debug.WriteLine(message);
+#endif
+		}
+		#endregion
+
+		#region Misc logic
+		private void ConnectToMSPN()
+		{
+			try
+			{
+                //First, try to pick up existing channel
+                httpChannel = HttpNotificationChannel.Find(channelName);
+
+                if (null != httpChannel)
+                {
+                    Trace("Channel Exists - no need to create a new one");
+                    SubscribeToChannelEvents();
+
+                    Trace("Register the URI with 3rd party web service");
+                    SubscribeToService();   //TODO: Place Notification
+
+                    Dispatcher.BeginInvoke(() => UpdateStatus("Channel recovered"));
+                }
+                else
+                {
+                    Trace("Trying to create a new channel...");
+                    //Create the channel
+                    httpChannel = new HttpNotificationChannel(channelName, "HOLWeatherService");
+                    Trace("New Push Notification channel created successfully");
+
+                    SubscribeToChannelEvents();
+
+                    Trace("Trying to open the channel");
+                    httpChannel.Open();
+                    Dispatcher.BeginInvoke(() => UpdateStatus("Channel open requested"));
+                }
+			}
+			catch ( Exception ex )
+			{
+				Dispatcher.BeginInvoke(() => UpdateStatus("Channel error: " + ex.Message));
+			}
+		}
+		#endregion
+
+        #region Subscriptions
+        private void SubscribeToChannelEvents()
+        {
+            //Register to UriUpdated event - occurs when channel successfully opens
+            httpChannel.ChannelUriUpdated += new EventHandler<NotificationChannelUriEventArgs>(httpChannel_ChannelUriUpdated);
+            //Subscribed to Raw Notification
+            httpChannel.HttpNotificationReceived += new EventHandler<HttpNotificationEventArgs>(httpChannel_HttpNotificationReceived);
+
+            //general error handling for push channel
+            httpChannel.ErrorOccurred += new EventHandler<NotificationChannelErrorEventArgs>(httpChannel_ExceptionOccurred);
+        }
+        #endregion
+
+        private void SubscribeToService()
+        {
+            //Hardcode for solution - need to be updated in case the REST WCF service address change
+            string baseUri = "http://localhost:8000/RegirstatorService/Register?uri={0}";
+            string theUri = String.Format(baseUri, httpChannel.ChannelUri.ToString());
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += (s, e) =>
+            {
+                if (null == e.Error)
+                    Dispatcher.BeginInvoke(() => UpdateStatus("Registration succeeded"));
+                else
+                    Dispatcher.BeginInvoke(() => UpdateStatus("Registration failed: " + e.Error.Message));
+            };
+            client.DownloadStringAsync(new Uri(theUri));
+        }
+
+		#region Channel event handlers
+		void httpChannel_HttpNotificationReceived(object sender, HttpNotificationEventArgs e)
+		{
+			Trace("===============================================");
+			Trace("RAW notification arrived:");
+
+			//TODO - add parsing and UI updating logic here
+			Trace("===============================================");
+		}
+
+        void httpChannel_ChannelUriUpdated(object sender, NotificationChannelUriEventArgs e)
+        {
+            Trace("Channel opened. Got Uri:\n" + httpChannel.ChannelUri.ToString());
+
+            Trace("Subscribing to channel events");
+            SubscribeToService();
+
+            Dispatcher.BeginInvoke(() => UpdateStatus("Channel created successfully"));
+        }
+
+        void httpChannel_ExceptionOccurred(object sender, NotificationChannelErrorEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => UpdateStatus(e.ErrorType + " occurred: " + e.Message));
+        }
+		#endregion
+	}
+}
---
+++ Ex3-SecondaryTilesToastInput/Begin/WPPushNotification.TestClient/PushHandler.cs
@@ -0,0 +1,66 @@
+﻿// ----------------------------------------------------------------------------------
+// Microsoft Developer & Platform Evangelism
+// 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+// ----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+// ----------------------------------------------------------------------------------
+
+using System;
+using Microsoft.Phone.Notification;
+using System.Collections.Generic;
+using System.Windows.Threading;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Xml.Linq;
+using System.Net;
+
+namespace WPPushNotification.TestClient
+{
+    /// <summary>
+    /// Handles the various aspects of managing push notifications for the application.
+    /// </summary>
+    public class PushHandler
+    {
+        private HttpNotificationChannel httpChannel;
+        const string channelName = "WeatherUpdatesChannel";
+
+        private bool connectedToMSPN;
+        private bool connectedToServer;
+        private bool notificationsBound;
+
+        /// <summary>
+        /// Contains information about the locations displayed by the application.
+        /// </summary>
+        public Dictionary<string, LocationInformation> Locations { get; private set; }
+
+        /// <summary>
+        /// A dispatcher used to interact with the UI.
+        /// </summary>
+        public Dispatcher Dispatcher { get; private set; }
+
+        /// <summary>
+        /// Push service related status information.
+        /// </summary>
+        public Status PushStatus { get; private set; }
+
+        /// <summary>
+        /// Whether or not the handler has fully established a connection to both the MSPN and the application server.
+        /// </summary>
+        public bool ConnectionEstablished
+        {
+            get
+            {
+                return connectedToMSPN && connectedToServer && notificationsBound;
+            }
+        }
+	}
+}
